2d327660f63a50609eb96c12e66edc7275018b1b
Shahin <meshahin007@gmail.com>
2019-03-05 21:31:55 +0600

    commit

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-var a = 10;
+// var a = 10;
 
-if(a <= 10){
-    console.log("Its true");
+// if(a <= 10){
+//     console.log("Its true");
+// } else{
+//     console.log("Its false");
+// }
+
+
+var age = 22;
+var gender = "Female";
+var gender = "Male";
+
+if(age >=21 && gender=="Male"){
+    console.log("He can get married");
+} 
+else if(age >=18 && gender=="Female"){
+    console.log("She can get married");
 } else{
-    console.log("Its false");
+    console.log("They cant get married");
 }
