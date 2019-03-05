94a8f8f1fed642f1d7cd6fb7272f61381c307d38
Shahin <meshahin007@gmail.com>
2019-03-05 22:03:10 +0600

    commit

--- index.js
+++ index.js
@@ -7,15 +7,34 @@
 // }
 
 
-var age = 22;
-var gender = "Female";
-var gender = "Male";
+// var age = 22;
+// var gender = "Female";
+// var gender = "Male";
 
-if(age >=21 && gender=="Male"){
-    console.log("He can get married");
-} 
-else if(age >=18 && gender=="Female"){
-    console.log("She can get married");
-} else{
-    console.log("They cant get married");
-}
+// if(age >=21 && gender=="Male"){
+//     console.log("He can get married");
+// } 
+// else if(age >=18 && gender=="Female"){
+//     console.log("She can get married");
+// } else{
+//     console.log("They cant get married");
+// }
+
+
+
+// while loop
+// var a = 0;
+// while(a<5){
+//     console.log(a);
+//     a++;
+//     console.log("BOO");
+// }
+// console.log("hello");
+
+
+
+// if(!true){
+//     console.log("Hello World");
+// } else{
+//     console.log("Not true");
+// }
